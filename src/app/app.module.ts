6d46293e872bf732d0831c812b23ca6f34f382a7
dime_coder <karthick.112000@outlook.com>
2022-10-20 18:20:31 +0530

    thusday commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import { DogComponent } from './dog/dog.component';
+import { CatComponent } from './cat/cat.component';
+import { RatComponent } from './rat/rat.component';
+
+
+
+const routes: Routes = [
+{path:'dog' ,component:DogComponent},
+{path:'cat' ,component:CatComponent},
+{path:'rat' ,component:RatComponent}
+]
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DogComponent,
+    CatComponent,
+    RatComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    RouterModule.forRoot(routes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/rat/rat.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RatComponent } from './rat.component';
+
+describe('RatComponent', () => {
+  let component: RatComponent;
+  let fixture: ComponentFixture<RatComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RatComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RatComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
