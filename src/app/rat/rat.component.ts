29a2c2de97abb70c05572238a08ee66f18b6e08e
dime_coder <karthick.112000@outlook.com>
2022-10-25 12:49:46 +0530

    25 commit

--- src/app/rat/rat.component.ts
+++ src/app/rat/rat.component.ts
@@ -1,3 +1,4 @@
+import { STRING_TYPE } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rat.component.css']
 })
 export class RatComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit(): void {
